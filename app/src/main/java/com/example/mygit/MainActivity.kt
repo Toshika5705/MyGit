57fe5fc04db2cff9ef2b723507d51f5789725ade
浚宏 蔡 <allen@hugsys.com>
2023-04-18 12:00:10 +0800

    測試2

--- app/src/main/java/com/example/mygit/MainActivity.kt
+++ app/src/main/java/com/example/mygit/MainActivity.kt
@@ -2,17 +2,13 @@ package com.example.mygit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Git()
-    }
-    fun Git(){
-        val app = 1
-        Log.d("T",app.toString())
+
     }
 }
